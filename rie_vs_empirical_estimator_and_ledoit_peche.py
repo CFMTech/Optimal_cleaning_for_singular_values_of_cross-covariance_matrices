0cae79d51ed7deca79e5b5c391b870c9e7f4f835
FloBenGeo <42642305+FloBenGeo@users.noreply.github.com>
2021-11-14 03:08:19 +0100

    Add files via upload

--- rie_vs_empirical_estimator_and_ledoit_peche.py
+++ rie_vs_empirical_estimator_and_ledoit_peche.py
@@ -3,28 +3,47 @@ Created on Thu Aug 16 22:42:48 2018
 
 @author: florent
 """
-
 from base_functions import *
 
-t0prog = time()
-a, b = .2, .5
-alpha, beta = .4, .7
-eps_positive = 1e-3  # t0 make sure we only use positive matrices as covariance matrices
-N_LGN = 100
+N_LGN = 50
 T = 500
-sigmatwo = .5
+print(f"T={T} and sample size={N_LGN}: for larger matrices and samples, parallelized jobs are more adapted")
 proportion_infos = [0., .1, .2, .3, .4]
+alphas_ht = ['5', '2.5', '1.5', '0.5']
+
+tested_cases = ([f'lin_model__{prop}' for prop in proportion_infos] +
+                ['wishart__2'] +
+                [f'heavy_tailed__{al}' for al in alphas_ht])
 
 
+def error_norm(x, x_0, norm='frobenius'):
+    assert norm in ('frobenius', 'operator_norm'), norm
+    return np.linalg.norm(x - x_0, ord=None if norm == 'frobenius' else 2)
 
 
-def my_model(T):
+def estimator_comparison(T=500, model_param='lin_model__0.1', alpha=.4, beta=.7, sigmatwo=.5, a=.2, b=.5):
     n, p = my_int(alpha * T), my_int(beta * T)
-    return (n, p, Wishart(n=n+p, p=2*(n+p), real=True))
-
-
-def diff(x, x_0, ord=None):
-    return np.linalg.norm(x - x_0, ord=ord)
+    model, param = tuple(model_param.split('__'))
+    assert model in ('lin_model', 'wishart', 'heavy_tailed'), model
+    if model == 'lin_model':
+        pp = my_int(float(param) * n)
+        C = np.matrix(Rectangular_Real_SRT(n, p, s=list(np.linspace(a, b, pp)) + [0] * (n - pp)))
+        TrueTotalCovariance = np.matrix(np.bmat([[np.eye(n), C], [C.T, (C.T) * C + sigmatwo * np.eye(p)]]))
+    elif model == 'wishart':
+        TrueTotalCovariance = Wishart(n=n + p, p=int(param) * (n + p), real=True)
+    else:
+        TrueTotalCovariance = Heavy_Tailed_Empirical_Covariance_Matrix(n=n + p, p=2 * (n + p), alpha=float(param))
+    C = TrueTotalCovariance[np.ix_(range(n), range(n, n + p))]
+    Etotale = Empirical_Covariance(T, TrueTotalCovariance)
+    paper_RIE = RIE_Cross_Covariance(Etotale, T, n, p)
+    projected_RIE = RIE_Covariance(Etotale, T)[np.ix_(range(n), range(n, n + p))]
+    emp_estim = Etotale[np.ix_(range(n), range(n, n + p))]
+    res = {}
+    for norm in ('frobenius', 'operator_norm'):
+        res[norm] = dict(emp=error_norm(emp_estim, C, norm),
+                         rie_paper=error_norm(paper_RIE, C, norm),
+                         proj_ledoit_peche=error_norm(projected_RIE, C, norm))
+    return pd.DataFrame(res)
 
 
 def Affichage(L, my_name):
@@ -33,38 +52,34 @@ def Affichage(L, my_name):
     print("\t\t%.2e   +/-   %.1e" % (x, 1.96 * y * N_LGN ** (-.5)))
 
 
+for model_param in tested_cases:
+    t0prog = time()
+    diff_RIE = []
+    diff_Emp = []
+    diff_RIE_Total = []
+    diff_RIE_operator_norm = []
+    diff_Emp_operator_norm = []
+    diff_RIE_Total_operator_norm = []
+    for i in range(N_LGN):
+        res = estimator_comparison(T=T, model_param=model_param)
+        diff_Emp.append(res.loc['emp', 'frobenius'])
+        diff_RIE.append(res.loc['rie_paper', 'frobenius'])
+        diff_RIE_Total.append(res.loc['proj_ledoit_peche', 'frobenius'])
+        diff_Emp_operator_norm.append(res.loc['emp', 'operator_norm'])
+        diff_RIE_operator_norm.append(res.loc['rie_paper', 'operator_norm'])
+        diff_RIE_Total_operator_norm.append(res.loc['proj_ledoit_peche', 'operator_norm'])
 
-diff_RIE = []
-diff_Emp = []
-diff_RIE_Total = []
-diff_RIE_operator_norm = []
-diff_Emp_operator_norm = []
-diff_RIE_Total_operator_norm = []
-for i in range(N_LGN):
-    (n, p, TrueTotalCovariance) = my_model(T)
-    C = TrueTotalCovariance[np.ix_(range(n), range(n, n + p))]
-    Etotale = Empirical_Covariance(T, TrueTotalCovariance)
-    paper_RIE = RIE_Cross_Covariance(Etotale, T, n, p)
-    projected_RIE = RIE_Covariance(Etotale, T)[np.ix_(range(n), range(n, n + p))]
-    emp_estim=Etotale[np.ix_(range(n), range(n, n + p))]
-    diff_Emp.append(diff(emp_estim, C))
-    diff_RIE.append(diff(paper_RIE, C))
-    diff_RIE_Total.append(diff(projected_RIE, C))
-    diff_Emp_operator_norm.append(diff(emp_estim, C, ord=2))
-    diff_RIE_operator_norm.append(diff(paper_RIE, C, ord=2))
-    diff_RIE_Total_operator_norm.append(diff(projected_RIE, C, ord=2))
-print("\n\n Wishart :")
-print("\n\tFrobenius norm:")
-Affichage(np.array(diff_RIE) / np.array(diff_Emp), "RIE Cross Cov/Emp:")
-Affichage(np.array(diff_RIE) / np.array(diff_RIE_Total), "RIE Cross Cov/RIE Ledoit Peche:")
-print("\n\tOperator norm:")
-Affichage(np.array(diff_RIE_operator_norm) / np.array(diff_Emp_operator_norm), "RIE Cross Cov/Emp:")
-Affichage(np.array(diff_RIE_operator_norm) / np.array(diff_RIE_Total_operator_norm), "RIE Cross Cov/RIE Ledoit Peche:")
+    print(f"\n\n {model_param} :")
+    print("\n\tFrobenius norm:")
+    Affichage(np.array(diff_RIE) / np.array(diff_Emp), "RIE Cross Cov/Emp:")
+    Affichage(np.array(diff_RIE) / np.array(diff_RIE_Total), "RIE Cross Cov/RIE Ledoit Peche:")
+    print("\n\tOperator norm:")
+    Affichage(np.array(diff_RIE_operator_norm) / np.array(diff_Emp_operator_norm), "RIE Cross Cov/Emp:")
+    Affichage(np.array(diff_RIE_operator_norm) / np.array(diff_RIE_Total_operator_norm),
+              "RIE Cross Cov/RIE Ledoit Peche:")
 
-Tprog = time() - t0prog
-if Tprog < 60:
-    print("\n\nTime in seconds : " + str(round(100 * Tprog) / 100.))
-else:
-    print("\n\nTime in minutes : " + str(round(10 * Tprog / 60) / 10.))
-# if Tprog>30:
-#     os.system('say "Programme fini"')
+    Tprog = time() - t0prog
+    if Tprog < 60:
+        print(f"\n\nTime in seconds for {model_param}: " + str(round(Tprog, 2)))
+    else:
+        print(f"\n\nTime in minutes for {model_param}: " + str(round(Tprog / 60, 1)))
